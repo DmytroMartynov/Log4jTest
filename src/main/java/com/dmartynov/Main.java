a08a4e58504ad9c3a56a75cd2782fdd96625a6b5
Dima Martynov <dmytro.martynov96@gmail.com>
2019-05-20 13:54:34 +0300

    log is done

---
+++ src/main/java/com/dmartynov/Main.java
@@ -0,0 +1,37 @@
+package com.dmartynov;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class Main {
+
+    private static final Logger rootLogger = LogManager.getRootLogger();
+    private static final Logger userLogger = LogManager.getLogger(User.class);
+
+    public static void main(String[] args) {
+        User user = new User();
+        user.setName("Anakin");
+        user.setLastName("Skywalker");
+
+        userLogger.info(user.showMeMessage());
+        userLogger.info(user.giveMeASign());
+
+        rootLogger.info("Root Logger: "  + user.showMeMessage());
+
+        //debug
+        if (rootLogger.isDebugEnabled()) {
+            rootLogger.debug("RootLogger: In debug message");
+            userLogger.debug("UserLogger in debug");
+        }
+
+        try {
+            User userNull = new User();
+            userNull.getName().toString();
+        } catch (NullPointerException ex) {
+            userLogger.error("error message: " + ex.getMessage());
+            userLogger.fatal("fatal error message: " + ex.getMessage());
+        }
+
+
+    }
+}
